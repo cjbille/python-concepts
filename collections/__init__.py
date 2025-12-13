0de22226ded88368cfc231027b19fd1484c71b7f
Chris Bille <cbille1@icloud.com>
2025-12-13 12:36:28 -0500

    Add collection info

--- collections/__init__.py
+++ collections/__init__.py
@@ -1,4 +1,11 @@
-fruits = [ "oranges", "apples", "apples", "blueberries", "bananas", "bananas", "strawberries" ] # list -> indexed, ordered, allow duplicates, mutable
-employee_emails = {"alice@corporation.com", "bob@corporation.com", "devantre@corporation.com"} # set -> not indexed, unordered, no duplicates, mutable
-coordinate = (40.7128, -74.0060) # tuple -> indexed, ordered, allow duplicates, immutable
-employee1 = {"name": "Alice", "age": 30, "email": "alice@corporation.com"} # dictionary -> not indexed, unordered, keys must be unique, mutable
+# list - indexed, ordered, allow duplicates, mutable
+fruits = [ "oranges", "apples", "apples", "blueberries", "bananas", "bananas", "strawberries" ]
+
+# set - not indexed, unordered, no duplicates, mutable
+employee_emails = {"alice@corporation.com", "bob@corporation.com", "devantre@corporation.com"}
+
+# tuple - indexed, ordered, allow duplicates, immutable
+coordinate = (40.7128, -74.0060)
+
+# dictionary - not indexed, unordered, keys must be unique, mutable
+employee_info = {"name": "Alice", "age": 30, "email": "alice@corporation.com"}
